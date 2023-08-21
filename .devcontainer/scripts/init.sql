a65fce816c1f734b44756d980c1f8cb2422d091d
Matthew White <matt.white@readytech.io>
2023-08-21 15:06:34 +1000

    Fixes devcontainer configuration (#23)
    ## Description
    
    It's been a little while since I last checked the state of this project
    and it seems the devcontainer configuration is broken. This PR fixes
    just that.
    
    Fixes # (issue)
    
    ## Type of change
    
    Please select the relevant option.
    
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Other
    
    ## Checklist
    
    Please review the following checklist and ensure all tasks are completed
    before submitting the pull request:
    
    - [x] I have performed a self-review of my own code
    - [ ] I have added tests that prove my fix is effective or that my
    feature works

--- .devcontainer/scripts/init.sql
+++ .devcontainer/scripts/init.sql
@@ -1,5 +1,5 @@
--- Currently, the SQLTOOLS extension does not support MySQL8 authentication, hence `mysql_native_password`
 CREATE USER 'vscode'@'%' IDENTIFIED WITH mysql_native_password BY 'vscode';
-GRANT ALL PRIVILEGES ON *.* TO 'vscode'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'vscode'@'%';
 CREATE DATABASE vscode;
-ALTER DATABASE vscode OWNER TO 'vscode';
+GRANT ALL PRIVILEGES ON vscode.* TO 'vscode'@'%';
+FLUSH PRIVILEGES;
